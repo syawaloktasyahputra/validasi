8ad9db269af2d2dc30ca2582dcf1f6dd5ade3ec7
syawaloktasyahputra <syawaloktasyahputra@gmail.com>
2025-10-26 20:45:03 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const DB_FILE = './database.json';
+const PORT = process.env.PORT || 3000;
 
-// helper
+// Helper functions
 function loadDB() {
   if(!fs.existsSync(DB_FILE)) fs.writeFileSync(DB_FILE, '[]');
   return JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
@@ -19,14 +22,23 @@ function saveDB(db) {
 
 // --- API CRUD ---
 
-// GET all
-app.get('/accounts', (req,res)=>{
-  const db = loadDB();
-  res.json(db);
+// GET all accounts
+app.get('/accounts', (req, res) => {
+  res.json(loadDB());
 });
 
-// POST add
-app.post('/accounts', (req,res)=>{
+// GET account by username
+app.get('/accounts/user/:username', (req, res) => {
+  const db = loadDB();
+  const user = db.find(u => u.username === req.params.username);
+  res.json(user || {});
+});
+
+// POST add new account
+app.post('/accounts', (req, res) => {
+  const { username, password } = req.body;
+  if(!username || !password) return res.status(400).json({ error: 'username & password required' });
+
   const db = loadDB();
   const newAcc = { _id: uuidv4(), ...req.body };
   db.push(newAcc);
@@ -34,29 +46,23 @@ app.post('/accounts', (req,res)=>{
   res.json(newAcc);
 });
 
-// PUT update
-app.put('/accounts/:id', (req,res)=>{
+// PUT update account
+app.put('/accounts/:id', (req, res) => {
   const db = loadDB();
-  const ix = db.findIndex(x=>x._id===req.params.id);
-  if(ix===-1) return res.status(404).json({error:'Not found'});
+  const ix = db.findIndex(x => x._id === req.params.id);
+  if(ix === -1) return res.status(404).json({ error: 'Not found' });
   db[ix] = { ...db[ix], ...req.body };
   saveDB(db);
   res.json(db[ix]);
 });
 
-// DELETE
-app.delete('/accounts/:id', (req,res)=>{
+// DELETE account
+app.delete('/accounts/:id', (req, res) => {
   let db = loadDB();
-  db = db.filter(x=>x._id!==req.params.id);
+  db = db.filter(x => x._id !== req.params.id);
   saveDB(db);
-  res.json({success:true});
+  res.json({ success: true });
 });
 
-// optional: find by username
-app.get('/accounts/user/:username', (req,res)=>{
-  const db = loadDB();
-  const user = db.find(u => u.username === req.params.username);
-  res.json(user || {});
-});
-
-app.listen(3000, ()=>console.log('Server running on http://localhost:3000'));
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
